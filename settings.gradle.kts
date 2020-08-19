fdbdbc4b7f3bcc1e77156ef0b8480540b74ac4cb
Sergey Okatov <sokatov@gmail.com>
2020-08-19 11:40:51 +0500

    M2L3-step5 Генерация проекта с OpenAPI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,28 @@ rootProject.name = "otuskotlin-user"
 pluginManagement {
     plugins {
         val kotlinVersion: String by settings
+        val openapiVersion: String by settings
+        val springVersion: String by settings
+        val springDependencyVersion: String by settings
 
         kotlin("multiplatform") version kotlinVersion apply false
         kotlin("jvm") version kotlinVersion apply false
         kotlin("js") version kotlinVersion apply false
         kotlin("plugin.serialization") version kotlinVersion apply false
+        id("org.openapi.generator") version "4.3.1" apply false
+        id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
+        id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+        id("org.springframework.boot") version springVersion
+        id("io.spring.dependency-management") version springDependencyVersion
+
+    }
+
+    repositories {
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        maven { url = uri("https://repo.spring.io/milestone") }
+        gradlePluginPortal()
+        mavenCentral()
+        jcenter()
     }
 }
 
@@ -19,3 +36,4 @@ include("ok-user-be-transport-multiplatform")
 include("ok-user-be-app-jetty")
 include("ok-user-fe-transport-multiplatform")
 include("ok-user-fe-angular")
+include("ok-user-oa-transport-models-jvm")
