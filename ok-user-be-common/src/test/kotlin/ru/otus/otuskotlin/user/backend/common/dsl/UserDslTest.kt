6c6bf9359167150ddb44cb60dfb2872298cef3b5
Sergey Okatov <sokatov@gmail.com>
2020-09-23 09:40:25 +0500

    M4L1 step 2 faulty test

--- ok-user-be-common/src/test/kotlin/ru/otus/otuskotlin/user/backend/common/dsl/UserDslTest.kt
+++ ok-user-be-common/src/test/kotlin/ru/otus/otuskotlin/user/backend/common/dsl/UserDslTest.kt
@@ -1,10 +1,31 @@
 package ru.otus.otuskotlin.user.backend.common.dsl
 
+import ru.otus.otuskotlin.user.backend.common.models.UserModel
+import java.time.LocalDate
 import kotlin.test.Test
 
 class UserDslTest {
     @Test
     fun userDslTest() {
+        val user: UserModel = user {
+            name {
+                first "Иван"
+                second "Иванович"
+                last "Ивавнов"
+            }
+            birth {
+                date LocalDate.parse("2000-01-01")
+            }
+            contracts {
+                email "ivan@ivanov.example"
+                phone "+7 999 999 9999"
+            }
+
+            permission {
+                permission "VIEW"
+                permission UserPermissionsModel.SEND_MESSAGE
+            }
+        }
 
     }
 }
