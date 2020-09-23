1a325dd20b9b6c261028827f7a3b702fa6cc1179
Sergey Okatov <sokatov@gmail.com>
2020-09-23 09:58:15 +0500

    M4L1 step 2 faulty test update

--- ok-user-be-common/src/test/kotlin/ru/otus/otuskotlin/user/backend/common/dsl/UserDslTest.kt
+++ ok-user-be-common/src/test/kotlin/ru/otus/otuskotlin/user/backend/common/dsl/UserDslTest.kt
@@ -1,6 +1,7 @@
 package ru.otus.otuskotlin.user.backend.common.dsl
 
 import ru.otus.otuskotlin.user.backend.common.models.UserModel
+import ru.otus.otuskotlin.user.backend.common.models.UserPermissionsModel
 import java.time.LocalDate
 import kotlin.test.Test
 
@@ -9,21 +10,23 @@ class UserDslTest {
     fun userDslTest() {
         val user: UserModel = user {
             name {
-                first "Иван"
-                second "Иванович"
-                last "Ивавнов"
+                first = "Иван"
+                second = "Иванович"
+                last = "Ивавнов"
             }
             birth {
-                date LocalDate.parse("2000-01-01")
+                date = LocalDate.parse("2000-01-01")
             }
             contracts {
-                email "ivan@ivanov.example"
-                phone "+7 999 999 9999"
+                email = "ivan@ivanov.example"
+                phone = "+7 999 999 9999"
             }
 
             permission {
-                permission "VIEW"
-                permission UserPermissionsModel.SEND_MESSAGE
+                add("VIEW")
+                add(UserPermissionsModel.SEND_MESSAGE)
+                + "UPDATE"
+                + UserPermissionsModel.GET_NEWS
             }
         }
 
