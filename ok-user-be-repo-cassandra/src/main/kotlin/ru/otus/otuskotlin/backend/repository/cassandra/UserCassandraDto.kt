8b36682c2b5f5a6d8f2dc76be045576c4a9b4b80
Sergey Okatov <sokatov@gmail.com>
2020-10-14 09:27:11 +0500

    M5L3 step2 создание репозитария

---
+++ ok-user-be-repo-cassandra/src/main/kotlin/ru/otus/otuskotlin/backend/repository/cassandra/UserCassandraDto.kt
@@ -0,0 +1,72 @@
+package ru.otus.otuskotlin.backend.repository.cassandra
+
+import com.datastax.driver.extras.codecs.jdk8.LocalDateCodec
+import com.datastax.driver.mapping.annotations.*
+import ru.otus.otuskotlin.backend.repository.cassandra.UserCassandraDto.Companion.TABLE_NAME
+import ru.otus.otuskotlin.user.backend.common.models.UserModel
+import java.time.Instant
+import java.time.LocalDate
+
+@Table(name = TABLE_NAME)
+data class UserCassandraDto(
+        @PartitionKey(0)
+        @Column(name = COLUMN_ID)
+        val id: String? = null,
+
+        @Column(name = COLUMN_NAME_FULL)
+        val fullName: String? = null,
+
+        @Column(name = COLUMN_NAME_FIRST)
+        val fname: String? = null,
+
+        @Column(name = COLUMN_NAME_MIDDLE)
+        val mname: String? = null,
+
+        @Column(name = COLUMN_NAME_LAST)
+        val lname: String? = null,
+
+        @Column(name = COLUMN_CONTACTS_PHONE)
+        val phone: String? = null,
+
+        @Column(name = COLUMN_CONTACTS_EMAIL)
+        val email: String? = null,
+
+        @Column(name = COLUMN_BIRTH_DATE, codec = LocalDateCodec::class)
+        val dob: LocalDate? = null,
+) {
+    fun toModel(): UserModel = UserModel(
+            id = id ?: "",
+            fname = fname ?: "",
+            mname = mname ?: "",
+            lname = lname ?: "",
+            dob = dob ?: LocalDate.MIN,
+            email = email ?: "",
+            phone = phone ?: "",
+    )
+
+    companion object {
+        const val TABLE_NAME = "users"
+        const val COLUMN_ID = "id"
+        const val COLUMN_NAME_FULL = "full_name"
+        const val COLUMN_NAME_FIRST = "fname"
+        const val COLUMN_NAME_MIDDLE = "mname"
+        const val COLUMN_NAME_LAST = "lname"
+        const val COLUMN_CONTACTS_PHONE = "phone"
+        const val COLUMN_CONTACTS_EMAIL = "email"
+        const val COLUMN_BIRTH_DATE = "dob"
+
+        fun of(user: UserModel) = of(user, user.id)
+
+        fun of(user: UserModel, id: String) = UserCassandraDto(
+                id = id.takeIf { it.isNotBlank() },
+                fullName = "${user.lname} ${user.fname} ${user.mname}",
+                fname = user.fname.takeIf { it.isNotBlank() },
+                mname = user.mname.takeIf { it.isNotBlank() },
+                lname = user.lname.takeIf { it.isNotBlank() },
+                phone = user.phone.takeIf { it.isNotBlank() },
+                email = user.email.takeIf { it.isNotBlank() },
+                dob = user.dob.takeIf { it != LocalDate.MIN },
+        )
+    }
+
+}
---
+++ ok-user-be-repo-cassandra/src/test/kotlin/ru/otus/otuskotlin/backend/repository/cassandra/UserRepositoryCassandraTest.kt
@@ -0,0 +1,123 @@
+package ru.otus.otuskotlin.backend.repository.cassandra
+
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.testcontainers.containers.GenericContainer
+import ru.otus.otuskotlin.user.backend.common.models.UserIndexFilter
+import ru.otus.otuskotlin.user.backend.common.models.UserModel
+import java.time.Duration
+import java.time.LocalDate
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class CassandraContainer : GenericContainer<CassandraContainer>("cassandra")
+
+internal class UserRepositoryCassandraTest {
+
+    companion object {
+        private val PORT = 9042
+        private val keyspace = "test_keyspace"
+        private lateinit var container: CassandraContainer
+        private lateinit var repo: UserRepositoryCassandra
+
+        @BeforeClass
+        @JvmStatic
+        fun tearUp() {
+            container = CassandraContainer()
+                    .withExposedPorts(PORT)
+                    .withStartupTimeout(Duration.ofSeconds(40L))
+                    .apply {
+                        start()
+                    }
+
+            repo = UserRepositoryCassandra(
+                    keyspace = keyspace,
+                    hosts = container.host,
+                    port = container.getMappedPort(PORT),
+                    initObjects = listOf(
+                            UserModel(id = "get-id", fname = "Ivan"),
+                            UserModel(id = "update-id"),
+                            UserModel(id = "delete-id", fname = "Ivan"),
+                            UserModel(id = "index-id-1", dob = LocalDate.parse("2020-01-01"), fname = "Ivan"),
+                            UserModel(id = "index-id-2", dob = LocalDate.parse("2020-01-01"), fname = "Petr"),
+                            UserModel(id = "index-id-3", dob = LocalDate.parse("2020-01-01"), fname = "John"),
+                    )
+            )
+
+            repo.init()
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun tearDown() {
+            container.close()
+        }
+    }
+
+    @Test
+    fun indexTest() {
+        runBlocking {
+            val obj = repo.index(UserIndexFilter(
+                    dob = "2020-01-01"
+            )).sortedBy { it.id }
+            assertEquals(listOf("index-id-1", "index-id-2", "index-id-3"), obj.map { it.id })
+            assertEquals("Ivan", obj.first().fname)
+        }
+    }
+
+    @Test
+    fun createTest() {
+        runBlocking {
+            val obj = repo.create(UserModel(
+                    fname = "Ivan",
+                    mname = "Ivanovich",
+                    lname = "Ivanov",
+                    dob = LocalDate.parse("2020-01-02"),
+                    phone = "+7 999 999 9999",
+                    email = "ivan@ivanov.example"
+            ))
+            assertNotNull(obj.id)
+            assertEquals("Ivan", obj.fname)
+            assertEquals("Ivanovich", obj.mname)
+            assertEquals("Ivanov", obj.lname)
+            assertEquals(LocalDate.parse("2020-01-02"), obj.dob)
+            assertEquals("+7 999 999 9999", obj.phone)
+            assertEquals("ivan@ivanov.example", obj.email)
+        }
+    }
+
+    @Test
+    fun updateTest() {
+        runBlocking {
+            val obj = repo.update(UserModel(
+                    id = "update-id",
+                    fname = "Ivan",
+                    mname = "Ivanovich",
+                    lname = "Ivanov",
+                    dob = LocalDate.parse("2020-01-02"),
+                    phone = "+7 999 999 9999",
+                    email = "ivan@ivanov.example"
+            ))
+            assertNotNull(obj.id)
+            assertEquals("Ivan", obj.fname)
+            assertEquals("Ivanovich", obj.mname)
+            assertEquals("Ivanov", obj.lname)
+            assertEquals(LocalDate.parse("2020-01-02"), obj.dob)
+            assertEquals("+7 999 999 9999", obj.phone)
+            assertEquals("ivan@ivanov.example", obj.email)
+        }
+    }
+
+    @Test
+    fun deleteTest() {
+        runBlocking {
+            val obj = repo.delete("delete-id")
+            assertEquals("delete-id", obj.id)
+            assertEquals("Ivan", obj.fname)
+        }
+    }
+
+}
